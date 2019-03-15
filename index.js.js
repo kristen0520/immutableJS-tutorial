ba5ae4059a5a71c7dececd6a74805c98053f9cff
kristen <kristenmay0520@gmail.com>
2019-03-14 23:14:17 -0400

    merge

--- index.js.js
+++ index.js.js
@@ -1,6 +1,8 @@
 const Immutable = require('immutable');
 
-var map1 = Immutable.Map({a: 1, b: 2, c:3})
-var map2 = map1.set('a', 100);
+var map1 = Immutable.Map({a: 1, b: 2, c:3});
+var map2 = Immutable.Map({d:4, e:5, f:6});
 
-console.log(map2)
+var map3 = map1.merge(map2);
+
+console.log(map3)
