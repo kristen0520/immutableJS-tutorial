aa3dbb3a94256e574f90a521176543c54e398b2d
kristen <kristenmay0520@gmail.com>
2019-03-14 23:20:27 -0400

    list

--- index.js.js
+++ index.js.js
@@ -2,9 +2,9 @@ const Immutable = require('immutable');
 
 var map1 = Immutable.Map({a: 1, b: 2, c:3});
 var map2 = Immutable.Map({d:4, e:5, f:6});
-
 var map3 = map1.merge(map2);
-
 var map4 = map1.delete('c');
 
-console.log(map4)
+var list1 = Immutable.List([1, 2, 3]);
+
+console.log(list1.get(1))
